2562dd24ea5496a833ac0bf850a1bbfa2e08753b
New <64774638+eric19740521@users.noreply.github.com>
2021-04-13 22:33:45 +0800

    1
    1

---
+++ Models/Member.cs
@@ -0,0 +1,12 @@
+
+
+using System;
+
+namespace Models 
+{
+public class Member
+{
+public int mem_no { get; set; }
+public string mem_name { get; set; }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Dapper;
+using Models;
+
+using System.Text;
+
+
+namespace test03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connStr = "Server=192.168.1.101;Database=testdb;User Id=SA;Password=<YourNewStrong@Passw0rd>;";
+
+            /*SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = "172.17.0.2";
+            builder.UserID = "sa";
+            builder.Password = "YourNewStrong@Passw0rd";
+            builder.InitialCatalog = "testdb";*/
+
+
+
+            using (var cn = new SqlConnection(connStr))
+            {
+                cn.Open();
+
+                string sql = "select * from member ";
+                var members = cn.Query<Member>(sql);
+
+                foreach (var _m in members)
+                {
+                    Console.WriteLine(_m.mem_no+"->"+_m.mem_name);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
